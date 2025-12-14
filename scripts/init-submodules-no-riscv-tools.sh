9bf3b6d0705ca563ed2ca630923f452220d19242
xianzhi <292224750@qq.com>
2025-12-14 15:36:02 +0800

    fix url bugs

--- scripts/init-submodules-no-riscv-tools.sh
+++ scripts/init-submodules-no-riscv-tools.sh
@@ -6,6 +6,10 @@ set -e
 set -o pipefail
 
 RDIR=$(git rev-parse --show-toplevel)
+cd ../generators
+rm -rf testchipip
+rm -rf rocket-chip
+git clone https://github.com/TONYSKYZENG/testchipip.git
+git clone https://github.com/TONYSKYZENG/rocket-chip.git
 cd "$RDIR"
-
 ./scripts/init-submodules-no-riscv-tools-nolog.sh "$@" 2>&1 | tee init-submodules-no-riscv-tools.log
